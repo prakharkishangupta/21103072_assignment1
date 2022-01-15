06cf756134727d142241c22fdb677d2a63a8de18
prakharkishangupta <97746974+prakharkishangupta@users.noreply.github.com>
2022-01-15 00:02:36 -0500

    Add files via upload

---
+++ 5_outputs.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================== RESTART: C:\Users\dell\pkg1.py ========================
+Enter first no =6
+Enter second no =5
+Enter third no =7
+Average of given no is = 6.0
+
+======================== RESTART: C:\Users\dell\pkg2.py ========================
+Enter gross income =100000
+Enter the no. of dependents =3
+Taxable income = 81000
+Tax = 16200.0
+
+======================== RESTART: C:\Users\dell\pkg3.py ========================
+Student ID;21103072
+Student Name:Prakhar Kumar Gupta
+Student Gender:M
+Student Course Name:CSE
+Student CGPA:9.6
+['21103072', 'Prakhar Kumar Gupta', 'M', 'CSE', '9.6']
+
+======================== RESTART: C:\Users\dell\pkg4.py ========================
+First Student Marks =78
+Second Student Marks =90
+Third Student Marks =92
+Forth Student Marks =95
+Fifth Student Marks =98
+List: ['78', '90', '92', '95', '98']
+Sorted List(decreasing order):
+List: ['98', '95', '92', '90', '78']
+
+======================== RESTART: C:\Users\dell\pkg5.py ========================
+(a)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+(b)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
