e4e2f996cc002108c47540b6beea3258b317ad60
prakharkishangupta <97746974+prakharkishangupta@users.noreply.github.com>
2022-02-15 02:48:29 -0500

    Add files via upload

---
+++ Assn3_Output.py
@@ -0,0 +1,126 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: C:\Users\dell\assn3-Q1.py ======================
+Give a string:prakharkumar
+{'p': 1, 'r': 3, 'a': 3, 'k': 2, 'h': 1, 'u': 1, 'm': 1}
+Done!
+
+====================== RESTART: C:\Users\dell\assn3-Q2.py ======================
+Give month:12
+Give day:31
+Give year:2021
+Date: 31 / 12 / 2021
+Next Date: 1/1 2022
+Done!
+
+====================== RESTART: C:\Users\dell\assn3-Q3.py ======================
+[(2, 4), (4, 16), (8, 64), (16, 256), (32, 1024)]
+Done!
+
+
+====================== RESTART: C:\Users\dell\assn3-Q4.py ======================
+Give a no. between 4 and 10 including them:8
+Performance=Very Good
+Letter Grade=B+
+Done!
+
+====================== RESTART: C:\Users\dell\assn3-Q4.py ======================
+Give a no. between 4 and 10 including them:11
+Error
+Done!
+
+====================== RESTART: C:\Users\dell\assn3-Q5.py ======================
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Done!
+
+====================== RESTART: C:\Users\dell\assn3-Q6.py ======================
+    (a)    
+Give the sid:21103072
+Enter your name:Prakhar
+Give a letter y or n:y
+Give the sid:21103073
+Enter your name:Kishan
+Give a letter y or n:y
+Give the sid:21103074
+Enter your name:Yash
+Give a letter y or n:n
+The required dictionary: {21103072: 'Prakhar', 21103073: 'Kishan', 21103074: 'Yash'}
+The list will be used in furder parts:
+The required list: [(21103072, 'Prakhar'), (21103073, 'Kishan'), (21103074, 'Yash')]
+Done!
+    (b)    
+The dictionary we had:
+{21103072: 'Prakhar', 21103073: 'Kishan', 21103074: 'Yash'}
+now we have exchanged the key value pair but its not sorted:
+{'Prakhar': 21103072, 'Kishan': 21103073, 'Yash': 21103074}
+The unsorted list:
+[('Prakhar', 21103072), ('Kishan', 21103073), ('Yash', 21103074)]
+The sorted list:
+[('Kishan', 21103073), ('Prakhar', 21103072), ('Yash', 21103074)]
+The sorted dictionary:
+{'Kishan': 21103073, 'Prakhar': 21103072, 'Yash': 21103074}
+This is the dictionary sorted on the basis of name:
+{21103073: 'Kishan', 21103072: 'Prakhar', 21103074: 'Yash'}
+Done!
+    (c)   
+The list we made before will be used now:
+[(21103072, 'Prakhar'), (21103073, 'Kishan'), (21103074, 'Yash')]
+now the list is sorted:
+[(21103072, 'Prakhar'), (21103073, 'Kishan'), (21103074, 'Yash')]
+Sorted dictionary based on sid: {21103072: 'Prakhar', 21103073: 'Kishan', 21103074: 'Yash'}
+Done!
+    (d)    
+Enter one of the sids you entered before:21103072
+Name of the student with the entered sid:
+Prakhar
+Done!
+
+====================== RESTART: C:\Users\dell\assn3-Q7.py ======================
+Give first number:2
+Give second number:4
+[2, 4, 6]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10, 16]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10, 16, 26]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10, 16, 26, 42]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10, 16, 26, 42, 68]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10, 16, 26, 42, 68, 110]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10, 16, 26, 42, 68, 110, 178]
+Give y to continue and n to stop going further:y
+[2, 4, 6, 10, 16, 26, 42, 68, 110, 178, 288]
+Give y to continue and n to stop going further:n
+Now we got a list having a fibonacci series:
+[2, 4, 6, 10, 16, 26, 42, 68, 110, 178, 288]
+Average of the sequence:
+68.18181818181819
+Done!
+
+====================== RESTART: C:\Users\dell\assn3-Q8.py ======================
+*(a)*
+{1, 3, 5, 6, 8}
+Done!
+*(b)*
+{3, 6, 8, 9, 13, 17}
+Done!
+*(c)*
+{1, 2, 4, 5}
+Done!
+*(d)*
+{6, 7, 8, 9, 10}
+Done!
+*(e)*
+{10, 7}
+Done!
